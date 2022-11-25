a70f36a7ffd126f2892201725b22d287516ed815
binding1 <jaey.pro@gmail.com>
2022-11-25 00:32:08 +0000

    added random string generator for short URLS

--- express_server.js
+++ express_server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+function generateRandomString() {
+  let randomUrl;
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  for (let i = 0; i < 6; i++) {
+    randomUrl += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return randomUrl;
+}
+
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -12,6 +24,11 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
